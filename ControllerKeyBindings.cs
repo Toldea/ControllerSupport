699459517c33e88df9c2dcca93a5b7a64aff6a08
Lex Offerman <lex.offerman@gmail.com>
2013-07-11 15:13:40 +0200

    Added OSX PS3 Keybindings
    - Added correct keybindings for a PS3 controller on OSX. A bool flag
    determines if we are using a ps3 or a 360 controller, however this
    still needs to be settable somewhere in the game.

--- ControllerKeyBindings.cs
+++ ControllerKeyBindings.cs
@@ -3,6 +3,7 @@ using System;
 namespace ControllerSupport {
 	public class ControllerKeyBindings {
 		private OSType osType;
+		private bool useOSX_PS3 = false;
 		public ControllerKeyBindings () {
 			osType = OsSpec.getOS ();
 		}
@@ -17,48 +18,132 @@ namespace ControllerSupport {
 
 		/**** Button Mappings ****/
 		public string A {
-			get { return (osType == OSType.Windows) ? "joystick button 0" : "joystick button 16" ; }
+			get { 
+				if (osType == OSType.Windows) {
+					return "joystick button 0";
+				} else {
+					return (useOSX_PS3) ? "joystick button 14" : "joystick button 16" ; 
+				}
+			}
 		}
 		public string B {
-			get { return (osType == OSType.Windows) ? "joystick button 1" : "joystick button 17" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "joystick button 1";
+				} else {
+					return (useOSX_PS3) ? "joystick button 13" : "joystick button 17" ; 
+				}
+			}
 		}
 		public string X {
-			get { return (osType == OSType.Windows) ? "joystick button 2" : "joystick button 18" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "joystick button 2";
+				} else {
+					return (useOSX_PS3) ? "joystick button 15" : "joystick button 18" ; 
+				}
+			}
 		}
 		public string Y {
-			get { return (osType == OSType.Windows) ? "joystick button 3" : "joystick button 19" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "joystick button 3";
+				} else {
+					return (useOSX_PS3) ? "joystick button 12" : "joystick button 19" ; 
+				}
+			}
 		}
 		public string LB {
-			get { return (osType == OSType.Windows) ? "joystick button 4" : "joystick button 13" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "joystick button 4";
+				} else {
+					return (useOSX_PS3) ? "joystick button 10" : "joystick button 13" ; 
+				}
+			}
 		}
 		public string RB {
-			get { return (osType == OSType.Windows) ? "joystick button 5" : "joystick button 14" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "joystick button 5";
+				} else {
+					return (useOSX_PS3) ? "joystick button 11" : "joystick button 14" ; 
+				}
+			}
 		}
 		public string BACK {
-			get { return (osType == OSType.Windows) ? "joystick button 6" : "joystick button 10" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "joystick button 6";
+				} else {
+					return (useOSX_PS3) ? "joystick button 0" : "joystick button 10" ; 
+				}
+			}
 		}
 		public string START {
-			get { return (osType == OSType.Windows) ? "joystick button 7" : "joystick button 9" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "joystick button 7";
+				} else {
+					return (useOSX_PS3) ? "joystick button 3" : "joystick button 9" ; 
+				}
+			}
 		}
 		public string LEFT_STICK_CLICK {
-			get { return (osType == OSType.Windows) ? "joystick button 8" : "joystick button 11" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "joystick button 8";
+				} else {
+					return (useOSX_PS3) ? "joystick button 1" : "joystick button 11" ; 
+				}
+			}
 		}
 		public string RIGHT_STICK_CLICK {
-			get { return (osType == OSType.Windows) ? "joystick button 9" : "joystick button 12" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "joystick button 9";
+				} else {
+					return (useOSX_PS3) ? "joystick button 2" : "joystick button 12" ; 
+				}
+			}
 		}
 
 		/**** OSX Only Buttons (DPad) ****/
 		public string DPAD_UP {
-			get { return (osType == OSType.Windows) ? "UNDEFINED" : "joystick button 5" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "UNDEFINED";
+				} else {
+					return (useOSX_PS3) ? "joystick button 4" : "joystick button 5" ; 
+				}
+			}
 		}
 		public string DPAD_DOWN {
-			get { return (osType == OSType.Windows) ? "UNDEFINED" : "joystick button 6" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "UNDEFINED";
+				} else {
+					return (useOSX_PS3) ? "joystick button 6" : "joystick button 6" ; 
+				}
+			}
 		}
 		public string DPAD_LEFT {
-			get { return (osType == OSType.Windows) ? "UNDEFINED" : "joystick button 7" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "UNDEFINED";
+				} else {
+					return (useOSX_PS3) ? "joystick button 7" : "joystick button 7" ; 
+				}
+			}
 		}
 		public string DPAD_RIGHT {
-			get { return (osType == OSType.Windows) ? "UNDEFINED" : "joystick button 8" ; }
+			get {
+				if (osType == OSType.Windows) {
+					return "UNDEFINED";
+				} else {
+					return (useOSX_PS3) ? "joystick button 5" : "joystick button 8" ; 
+				}
+			}
 		}
 	}
 }
